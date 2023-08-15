d8ded44a550ed6341373977b3978d8cf20178435
Niziyas <imedhamdiarchitects@gmail.com>
2023-08-15 04:05:09 -0700

    #include "main.h" /**  * main - Entry point  * Return: Always 0 (success)  */ int main(void) { char *sh = "_putchar";
    while (*sh)
    {
    _putchar(*sh);
    sh++;
    }
    _putchar(n);
    
    return (0);

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,18 +1,18 @@
 #include "main.h"
-
 /**
-*main - function prints text as output
-*
-*Return:return 0
-*/
+ * main - Entry point
+ * Return: Always 0 (success)
+ */
 int main(void)
 {
-char school[10] = "_putchar";
-int i;
-for (i = 0; i < 8; i++)
-{
-_putchar(school[i]);
-}
-_putchar(10);
-return (0);
+	char *word = "_putchar";
+
+	while (*word)
+	{
+		_putchar(*word);
+		word++;
+	}
+	_putchar('\n');
+
+	return (0);
 }
