e09af35950112a103b1135b51e5d7132ba4cb522
Niziyas <Imedhamdiarchitects@gmail.com>
2023-10-02 12:16:32 -1100

    dd

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -1,39 +1,29 @@
 #include "main.h"
-#include <unistd.h>
+
 /**
- * create_file - read text file
- * @filename: name of the file
- * @text_content: text content
- * Return: number of bytes read
+ * create_file - a function that creates a file
+ * @filename:  is the name of the file to create
+ * @text_content: is a NULL terminated string to write to the file
+ * Return: 1 on success, -1 on failure
  */
+
 int create_file(const char *filename, char *text_content)
 {
-int fd, check_len;
-size_t write_len;
+	int fd, i, j = 0;
 
-if (!filename)
-return (-1);
-if (!text_content)
-{
-text_content = "";
-write_len = 0; /*check this latter*/
-}
-else
-{
-write_len = 0;
-while (*(text_content + write_len))
-{
-write_len++;
-}
-
-}
-fd = open(filename, O_CREAT | O_RDWR | O_TRUNC, 0600);
-if (fd == -1)
-return (-1);
-check_len = write(fd, text_content, write_len);
-if (check_len == -1)
-return (-1);
-close(fd);
-return (1);
-
+	if (!filename)
+		return (-1);
+	fd = open(filename, O_CREAT | O_RDWR | O_TRUNC, 0600);
+	if (fd < 0)
+		return (-1);
+	if (text_content)
+	{
+		while (text_content[j])
+			++j;
+		i = write(fd, text_content, j);
+		if (i != j)
+			return (-1);
+	}
+	close(fd);
+	return (1);
 }
