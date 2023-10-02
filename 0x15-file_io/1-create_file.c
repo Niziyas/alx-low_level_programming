10f912675d237b64515dba47440f488184370a35
Niziyas <Imedhamdiarchitects@gmail.com>
2023-10-02 07:27:09 -1100

    ff

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -3,19 +3,24 @@
 #include <unistd.h>
 #include <fcntl.h>
 
-int create_file(const char *filename, char *text_content) {
-if (filename == NULL) {
+int create_file(const char *filename, char *text_content) 
+{
+if (filename == NULL)
+{
 return -1;
 }
 
 int fd = open(filename, O_CREAT | O_WRONLY | O_TRUNC, 0600);
-if (fd == -1) {
+if (fd == -1) 
+{
 return -1;
 }
 
-if (text_content != NULL) {
+if (text_content != NULL) 
+{
 ssize_t bytes_written = write(fd, text_content, strlen(text_content));
-if (bytes_written == -1) {
+if (bytes_written == -1)
+{
 close(fd);
 return -1;
 }
