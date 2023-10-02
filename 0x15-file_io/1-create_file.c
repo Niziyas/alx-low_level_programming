3eaa06e27a04527a3f3b5bdf6f54b5e622525ef4
Niziyas <Imedhamdiarchitects@gmail.com>
2023-10-02 07:10:44 -1100

    ee

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -1,51 +1,39 @@
 #include "main.h"
-#include <stddef.h>
-
+#include <unistd.h>
 /**
- * _strlen - counts string length
- * @str: string to be used
- *
- * Return: length of the string
- */
-int _strlen(char *str)
-{
-	int len = 0;
-
-	while (str[len] != '\0')
-		len++;
-	return (len);
-}
-
-/**
- * create_file - creates a file
+ * create_file - read text file
  * @filename: name of the file
- * @text_content: content of the file to be created
- *
- * Return: 1 on success, -1 otherwise
+ * @text_content: text content
+ * Return: number of bytes read
  */
 int create_file(const char *filename, char *text_content)
 {
-	int file, wrote;
+int fd, check_len;
+size_t write_len;
 
-	if (filename == NULL)
-		return (-1);
-	file = open(filename, O_WRONLY | O_CREAT | O_TRUNC, 0600);
-	if (file == -1)
-		return (-1);
-	if (text_content != NULL)
-	{
-		wrote = write(file, text_content, _strlen(text_content));
-		if (wrote == -1)
-		{
-			close(file);
-			return (-1);
-		}
-		close(file);
-		return (1);
-	}
-	else
-	{
-		close(file);
-		return (1);
-	}
+if (!filename)
+return (-1);
+if (!text_content)
+{
+text_content = "";
+write_len = 0; /*check this latter*/
+}
+else
+{
+write_len = 0;
+while (*(text_content + write_len))
+{
+write_len++;
+}
+
+}
+fd = open(filename, O_CREAT | O_RDWR | O_TRUNC, 0600);
+if (fd == -1)
+return (-1);
+check_len = write(fd, text_content, write_len);
+if (check_len == -1)
+return (-1);
+close(fd);
+return (1);
+
 }
