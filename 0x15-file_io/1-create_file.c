d3b5d16c632b9d2c7f0768e8b90504c121db0694
Niziyas <Imedhamdiarchitects@gmail.com>
2023-10-02 11:57:12 -1100

    ii

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -10,15 +10,15 @@ int main(int ac, char **av) {
 int res;
 
 if (ac != 3) {
-printf("Usage: %s <filename> <text>\n", av[0]);
-return (1);
+    printf("Usage: %s <filename> <text>\n", av[0]);
+    return (1);
 }
 
 res = create_file(av[1], av[2]);
 
 if (res == -1) {
-printf("Error: Could not create the file or write to it.\n");
-return (1);
+    printf("Error: Could not create the file or write to it.\n");
+    return (1);
 }
 
 printf("File created and written successfully.\n");
@@ -27,19 +27,19 @@ return (0);
 
 int create_file(const char *filename, char *text_content) {
 if (filename == NULL) {
-return -1;
+    return -1;
 }
 
 int fd = open(filename, O_CREAT | O_WRONLY | O_TRUNC, 0600);
 if (fd == -1) {
-return -1;
+    return -1;
 }
 
 if (text_content != NULL) {
-ssize_t bytes_written = write(fd, text_content, strlen(text_content));
+    ssize_t bytes_written = write(fd, text_content, strlen(text_content));
 if (bytes_written == -1) {
-close(fd);
-return -1;
+    close(fd);
+    return -1;
 }
 }
 
