82822aa88762fa9debfecaa28101a79da0562196
Niziyas <Imedhamdiarchitects@gmail.com>
2023-10-02 12:08:55 -1100

    tt

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -1,48 +1,39 @@
-#include <stdio.h>
-#include <stdlib.h>
+#include "main.h"
 #include <unistd.h>
-#include <fcntl.h>
-#include <string.h>  // Add this include for 'strlen'
+/**
+ * create_file - read text file
+ * @filename: name of the file
+ * @text_content: text content
+ * Return: number of bytes read
+ */
+int create_file(const char *filename, char *text_content)
+{
+int fd, check_len;
+size_t write_len;
 
-int create_file(const char *filename, char *text_content);
-
-int main(int ac, char **av) {
-int res;
-
-if (ac != 3) {
-    printf("Usage: %s <filename> <text>\n", av[0]);
-    return (1);
+if (!filename)
+return (-1);
+if (!text_content)
+{
+text_content = "";
+write_len = 0; /*check this latter*/
+}
+else
+{
+write_len = 0;
+while (*(text_content + write_len))
+{
+write_len++;
 }
 
-res = create_file(av[1], av[2]);
-
-if (res == -1) {
-    printf("Error: Could not create the file or write to it.\n");
-    return (1);
 }
-
-printf("File created and written successfully.\n");
-return (0);
-}
-
-int create_file(const char *filename, char *text_content) {
-if (filename == NULL) {
-    return -1;
-}
-
-int fd = open(filename, O_CREAT | O_WRONLY | O_TRUNC, 0600);
-if (fd == -1) {
-    return -1;
-}
-
-if (text_content != NULL) {
-    ssize_t bytes_written = write(fd, text_content, strlen(text_content));
-if (bytes_written == -1) {
-    close(fd);
-    return -1;
-}
-}
-
+fd = open(filename, O_CREAT | O_RDWR | O_TRUNC, 0600);
+if (fd == -1)
+return (-1);
+check_len = write(fd, text_content, write_len);
+if (check_len == -1)
+return (-1);
 close(fd);
-return 1;
+return (1);
+
 }
--- 0x15-file_io/_putchar.c
+++ 0x15-file_io/_putchar.c
@@ -6,8 +6,8 @@
  * @c: character to be printed
  * Return: 0 on success
  */
-int _putchar(char c)
+int _putchar (char c)
 {
-  return(write(1, &c, 1));
-  
+return (write(1, &c, 1));
+
 }
