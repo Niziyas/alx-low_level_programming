05a6edbf88a79cbfba35461a7e2b61a20a2aec42
Niziyas <Imedhamdiarchitects@gmail.com>
2023-10-02 07:17:36 -1100

    pp

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -1,39 +1,26 @@
-#include "main.h"
+#include <stdio.h>
+#include <stdlib.h>
 #include <unistd.h>
-/**
- * create_file - read text file
- * @filename: name of the file
- * @text_content: text content
- * Return: number of bytes read
- */
-int create_file(const char *filename, char *text_content)
-{
-int fd, check_len;
-size_t write_len;
+#include <fcntl.h>
 
-if (!filename)
-return (-1);
-if (!text_content)
-{
-text_content = "";
-write_len = 0; /*check this latter*/
-}
-else
-{
-write_len = 0;
-while (*(text_content + write_len))
-{
-write_len++;
-}
+int create_file(const char *filename, char *text_content) {
+    if (filename == NULL) {
+        return -1;
+    }
 
-}
-fd = open(filename, O_CREAT | O_RDWR | O_TRUNC, 0600);
-if (fd == -1)
-return (-1);
-check_len = write(fd, text_content, write_len);
-if (check_len == -1)
-return (-1);
-close(fd);
-return (1);
+    int fd = open(filename, O_CREAT | O_WRONLY | O_TRUNC, 0600);
+    if (fd == -1) {
+        return -1;
+    }
 
+    if (text_content != NULL) {
+        ssize_t bytes_written = write(fd, text_content, strlen(text_content));
+        if (bytes_written == -1) {
+            close(fd);
+            return -1;
+        }
+    }
+
+    close(fd);
+    return 1;
 }
