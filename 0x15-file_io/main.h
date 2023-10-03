d8926b80638da4cc4b2057e66da899a6ee268ee7
Niziyas <Imedhamdiarchitects@gmail.com>
2023-10-02 13:19:57 -1100

    ddd

--- 0x15-file_io/main.h
+++ 0x15-file_io/main.h
@@ -6,9 +6,12 @@
 #include <sys/stat.h>
 #include <fcntl.h>
 #include <unistd.h>
+#include <stdio.h> /* Added for printf, dprintf, and perror */
+#include <stdlib.h> /* Added for exit and malloc */
 
 ssize_t read_textfile(const char *filename, size_t letters);
 int create_file(const char *filename, char *text_content);
 int append_text_to_file(const char *filename, char *text_content);
+int main(int argc, char *argv[]);
 
 #endif /* MAIN_H */
